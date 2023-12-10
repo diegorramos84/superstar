867667ec265a0355c630cf586297fb99f3252135
Diego Ramos <diegorramos84@gmail.com>
2023-12-10 15:46:27 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Main template file
+ *
+ * @package superstar
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Wordpress Theme</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
